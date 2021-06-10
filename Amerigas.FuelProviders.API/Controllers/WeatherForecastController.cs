bd015be499c5286a63f7b597032053a847de3451
Hani Ho <hani.herbert.ho@gmail.com>
2021-06-10 22:02:34 +0800

    Finished bare-bones Azure Cosmos Insert.

---
+++ Amerigas.FuelProviders.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amerigas.FuelProviders.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private CosmosDbService _cosmosDbService;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _cosmosDbService = new CosmosDbService();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("Cosmos")]
+        public async Task<string> InsertToCosmos()
+        {
+            var myObject = new
+            {
+                id = Guid.NewGuid(),
+                Name = "Hani",
+                Level = 85,
+                Hobby = "Cooking",
+                FavoriteFood = "Lasagna",
+                FavoriteDay = "Monday",
+                Color = "Orange"
+            };
+            JObject o = (JObject)JToken.FromObject(myObject);
+            return await _cosmosDbService.CreateItem(o);
+        }
+    }
+}
---
+++ Amerigas.FuelProviders.API/CosmosDbService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Amerigas.FuelProviders.API
+{
+    public class CosmosDbService
+    {
+        private readonly string _databaseName = @"SampleDB";
+        private readonly string _containerName = @"Hani-Container";
+        private readonly string _account = @"https://amerigascosmostest.documents.azure.com:443/";
+        private readonly  string _key = @"5SGA8ft28dINUNGCwccpccI7FE1TvLVovlOPk1U5V3th71xTNYLR1E0SvwwbkpJCNF3x2vR6DVumy1jrWM8tIg==";
+        private readonly CosmosClient _client;
+        private Container _container;
+
+        public CosmosDbService()
+        {
+            _client = new CosmosClient(_account, _key);
+            _container = _client.GetContainer(_databaseName, _containerName);
+        }
+
+        public async Task<string> CreateItem(JObject newItem)
+        {
+            //Add new item in CosmosDB container only if it does not exist yet, check Id property
+            string result;
+            try
+            {
+                ItemResponse<JObject> itemResponse = await _container.ReadItemAsync<JObject>(newItem["id"].ToString(), new PartitionKey(newItem["id"].ToString()));
+                result = "Item with id " + newItem["id"].ToString() + " already exists.";
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                // Create an item in the container if it doesnt exist yet
+                ItemResponse<JObject> itemResponse = await _container.CreateItemAsync<JObject>(newItem, new PartitionKey(newItem["id"].ToString()));
+                result = "New item with id " + newItem["id"].ToString() + " created.";
+            }
+            catch (Exception ex)
+            {
+                result = "Error: " + ex.Message;
+            }
+
+            return result;
+        }
+
+    }
+}
